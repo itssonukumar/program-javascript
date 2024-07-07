af24dce32b618d21937574860239b8c226f5c2c9
itssonukumar <itssonukumar69@gmail.com>
2024-07-07 19:40:18 +0530

    commit

---
+++ eventlistner.js
@@ -0,0 +1,32 @@
+// callback function ; wecan passe into another function.
+
+
+// setTimeout(function(){
+//     console.log("timer");
+// },5000)
+
+// function x(y){ //y is called here.and we can call x anywher in the program.
+//     console.log("x");
+//     y();
+// }
+// x(function y(){     // x is called.
+//     console.log("y");
+// })
+
+function attachedeventlistner(){
+    let count=0;
+
+document.getElementById("clickme")
+.addEventListener("click",function xyz(){
+console.log("button clicked",++count);
+})
+}
+attachedeventlistner();
+
+
+// let count=0;  globally exucute count.
+
+// document.getElementById("clickme")
+// .addEventListener("click",function xyz(){
+// console.log("button clicked",++count);
+// })
